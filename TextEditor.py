bd2033374b915f203ed060555e57cccf414b0ee6
Aradhya Sharma <81953889+AS-Developer15@users.noreply.github.com>
2025-04-30 14:30:44 +0530

    Added new functionalities !!!

--- TextEditor.py
+++ TextEditor.py
@@ -1,83 +1,107 @@
+# Module Import 
+
 from ttkbootstrap import *
 from tkinter import *
 import webbrowser
 from random import choice
+
+# Main Function for our Editor Window
 def newWindow():
+
 # Sub Function Decleration
 
-    def webAbtUs():
-        webbrowser.open_new_tab("https://google.com")
+    def webAbtUs(): # Opens Github
+        webbrowser.open_new_tab("https://github.com/AS-Developer15")
+       
         
-    def helpProv():
-        webbrowser.open_new_tab("https://google.com")
-        
-    def themeChanger():
-        themeWindow= Tk()
-        Label(themeWindow,text="Choose Theme".title()).pack()
-        themeVal = Entry(themeWindow)
-        themeVal.pack()
-        strThemeVal= themeVal.get()
-        def changeTheme():
-            window.style.theme= strThemeVal
-            window.bell()  
-            themeWindow.destroy()
-            
-        Button(themeWindow,command=changeTheme,text="Change Theme").pack()
-        themeWindow.mainloop()
-    
-    
-    def formattingWin():
+    def documentation(): # Opens Documentation of the Project
+        webbrowser.open_new_tab("https://github.com/AS-Developer15/TextEditor")
+      
+      
+    def formattingWin(): # Opens a window like Notepad
 
         formattingWindow= Tk()
+        formattingWindow.title("Change Style")
         formattingWindow.geometry('450x300')
+        
         Label(formattingWindow,text="Change Configuration").pack(pady=10)
+       
         mainFrame = Frame(formattingWindow)         
         fontValFrame =  Frame(mainFrame)
         fontSizeFrame=  Frame(mainFrame)
         fontStylFrame=  Frame(mainFrame)
+        fontColrFrame=  Frame(mainFrame)
+        windBackFrame=  Frame(mainFrame)
         
-        Label(fontValFrame,text="Choose Font Face ").pack(side="left",pady=5,padx=3)
+        Label(fontValFrame,text="Face Value",width=12,anchor="w").pack(side="left",pady=5)
         fontVal =  Entry(fontValFrame)
         fontVal.pack(side= "left",padx=3)
         fontValFrame.pack()
                
-        Label(fontSizeFrame,text="Choose Font Size  ").pack(side="left",pady=5,padx=3)
+        Label(fontSizeFrame,text="Size Value",width=12,anchor="w").pack(side="left",pady=5)
         fontSizeVal =  Entry(fontSizeFrame)
         fontSizeVal.pack(side= "left",padx=3)
         fontSizeFrame.pack()
         
-        Label(fontStylFrame,text="Choose Font Style").pack(side="left",pady=5,padx=3)
+        Label(fontStylFrame,text="Style Value",width=12,anchor="w").pack(side="left",pady=5)
         fontStyleVal=  Entry(fontStylFrame)
         fontStyleVal.pack(side= "left",padx=3)        
         fontStylFrame.pack()
        
-        def changeConfig():
+        Label(fontColrFrame,text="Color Value",width=12,anchor="w").pack(side="left",pady=5)
+        fontColorVal=  Entry(fontColrFrame)
+        fontColorVal.pack(side= "left",padx=3)        
+        fontColrFrame.pack()
        
-            fontFaceValue = str(fontVal.get()).lower()
-            fontSizeValue = int(fontSizeVal.get())
-            fontWeightValue= str(fontStyleVal.get()).lower()
-        
+        Label(windBackFrame,text="Window Color",width=12,anchor="w").pack(side="left",pady=5)
+        windColorVal=  Entry(windBackFrame)
+        windColorVal.pack(side= "left",padx=3)        
+        windBackFrame.pack()
+       
+        def changeConfig(): # Sub Function for regulating all those changes made by the user
+
+            fontFaceValue   = str(fontVal.get()).lower()
+            fontSizeValue   = fontSizeVal.get()
+            fontWeightValue = str(fontStyleVal.get()).lower()
+            fontColorValue  = str(fontColorVal.get())
+            windowColorValue= str(windColorVal.get())
+          
+          
             if fontFaceValue == "":
                 fontFaceValue="calibri"
-           
-            if str(fontSizeValue)=="":
-                fontSizeValue=12
+            
+            if fontSizeValue=='':
+                fontSizeValue= int(12)
 
             if fontWeightValue == "":
                 fontWeightValue="normal"
+            
+            if fontColorValue=="":
+                fontColorValue= textEntryWindow.cget("foreground")
+           
+            if windowColorValue=="": 
+                windowColorValue=textEntryWindow.cget("background")
+        
                 
             fontConfig= font.Font(family=fontFaceValue,size=fontSizeValue,weight=fontWeightValue)
-            textEntryWindow.config(font=fontConfig)    
+            textEntryWindow.config(font=fontConfig)
+            textEntryWindow.config(foreground=fontColorValue)
+            textEntryWindow.config(background=windowColorValue)
+        
+            formattingWindow.bell()
             formattingWindow.destroy()
         mainFrame.pack()         
         Button(formattingWindow,text="Change Configuration",command=changeConfig).pack(pady=10)
         formattingWindow.mainloop()
     
-    def fileSave():
+    
+    def fileSave(): # Saving File using open command 
         nameWindow= Tk()
-        nameWindow.title("Save File")
-        
-        Label(nameWindow,text="Choose File Name").pack()
+        nameWindow.geometry("300x150")
+        nameWindow.title("Save File" )
+          
+        Label(nameWindow,text="Choose File Name",font=(24)).pack(pady=20)
+      
         fileSavingFrame = Frame(nameWindow)
         fileName = Entry(fileSavingFrame)
         fileName.pack(side="left")
@@ -85,20 +109,32 @@ def newWindow():
         def saveFile():
             savingName = fileName.get()   
             data = textEntryWindow.get(index1=1.0,index2="end")
+            
+            
+            # File Path to be changed in later updates.
+            
+             
             with open(f"./{savingName}.txt","w") as file:
-                file.write(data)    
-        Button(fileSavingFrame,text="Save",command=saveFile).pack(side="left")
+                file.write(data)
+            nameWindow.bell()  
+            nameWindow.destroy()  
+        Button(fileSavingFrame,text="Save",command=saveFile).pack(side="left",padx=10)
         nameWindow.mainloop()
 
-    def exitEditor():
+
+    def exitEditor(): # Destroying the window for exiting
+        window.bell()
         window.destroy()
+        
 
 # Program Window Configuration
 
     themes=['cosmo', 'flatly', 'litera', 'minty', 'lumen', 'sandstone', 'yeti', 'pulse', 'united', 'morph', 'journal', 'darkly', 'superhero', 'solar', 'cyborg', 'vapor', 'simplex', 'cerculean']
+   
     window = Window("Text Editor",themename=choice(themes))
+    window.geometry("1000x500")
     window.maxsize(window.winfo_screenwidth(),window.winfo_screenheight())
-    print(themes)
+
 
 
 # File Menu
@@ -111,24 +147,36 @@ def newWindow():
     menuBar.add_cascade(label="File",menu=fileMenu)
 
 
+
 # Editing Menu
     editingMenu= Menu(menuBar,tearoff=0)
     editingMenu.add_command(label="Edit",command=formattingWin)
-    editingMenu.add_command(label="BackGround",command=themeChanger)
     menuBar.add_cascade(label="Edit",menu=editingMenu)
+
 
 
 # Help Menu
     helpMenu = Menu(menuBar,tearoff=0 )
     helpMenu.add_command(label="About Us",command=webAbtUs)
-    helpMenu.add_command(label="Developer",command=helpProv) 
+    helpMenu.add_command(label="Documentation",command=documentation) 
     menuBar.add_cascade(menu=helpMenu,label="Help")
     window.config(menu=menuBar)
 
 
+
 # Actual Window
-    textEntryWindow = ScrolledText(window,background="white",width=200,height=25,font=('Algerian',12))
+    textEntryWindow = ScrolledText(window,background="white",width=200,height=25)
     textEntryWindow.pack(fill= tk.BOTH , expand=True)
     window.mainloop()
+
+
        
 newWindow()
+
+"""
+Future Updates Include 
+>> File Saving Path
+>> Word Wrapping
+>> Opening a Text File
+>> Cut, Copy, Paste, Menu
+"""
